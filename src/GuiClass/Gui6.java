e88d8e8eabbe4ff87515b9ce94cd833e95f3a3f6
Drys-H <drysluiggi@gmail.com>
2026-02-06 16:59:16 +0000

    week two adding the dif gui class

--- src/GuiClass/Gui6.java
+++ src/GuiClass/Gui6.java
@@ -4,6 +4,7 @@ package GuiClass;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,14 +14,14 @@ import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
 /**
- * Now a long running job running on a worker thread is sending updates
- * back to the UI - and these updates need to be sent as jobs scheduled
- * on the event dispatch queue.
+ * Long running job scheduling millions of updates on the event dispatch
+ * queue with no delay between them but using SwingUtilities.invokeAndWait
+ * to preserve UI responsiveness and ensure all updates are painted.
  */
-public class Gui4 extends JFrame {
+public class Gui6 extends JFrame {
   private final JButton button = new JButton( "Press me");
   private final JLabel infoLabel = new JLabel( "Ready...");
-  public Gui4() {
+  public Gui6() {
     setDefaultCloseOperation( WindowConstants.DISPOSE_ON_CLOSE);
     setTitle(this.getClass().getSimpleName());
     JPanel panel = new JPanel( new GridLayout(2,0));
@@ -41,20 +42,25 @@ public class Gui4 extends JFrame {
   private void doButton() {
     new Thread( new Runnable() {
       public void run() {
-        for( int i = 0; i < 100; i++) {
+        for( int i = 0; i < 10000000; i++) {
           queueInfo( "Ouch " + i + "!");
-          try { Thread.sleep( 500); } catch( InterruptedException x) {}
+          //try { Thread.sleep( 500); } catch( InterruptedException x) {}
         }
         queueInfo( "Done...");        
       }}).start();
   }
 
   public void queueInfo( final String info) {
-    SwingUtilities.invokeLater( new Runnable() {
-      @Override
-      public void run() {
-        setInfo( info);       
-      }});
+    try {
+      SwingUtilities.invokeAndWait( new Runnable() {
+        @Override
+        public void run() {
+          setInfo( info);       
+        }});
+    } catch (InvocationTargetException | InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
   
   public void setInfo( String info) {
@@ -65,7 +71,7 @@ public class Gui4 extends JFrame {
     SwingUtilities.invokeLater( new Runnable() {
       @Override
       public void run() {
-        new Gui4();
+        new Gui6();
       }
     });
   }
